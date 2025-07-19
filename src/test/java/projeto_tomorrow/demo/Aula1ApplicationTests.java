f4d6484c444e0ee946f1d10df3608bfafd17d995
Jackson <jreis3607@gmail.com>
2025-07-19 15:44:58 -0300

    feat: initial commit

---
+++ src/test/java/projeto_tomorrow/demo/Aula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_tomorrow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
